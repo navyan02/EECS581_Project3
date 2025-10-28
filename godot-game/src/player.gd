ed1ef532b7d545c4c6d13f4310ccd23ef5ec0410
Katie-N <kaitlyn.nordberg@gmail.com>
2025-10-27 19:52:41 -0500

    Made a simple scene and player movable

---
+++ godot-game/src/player.gd
@@ -0,0 +1,14 @@
+extends CharacterBody2D
+
+var posToGoTo : Vector2
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("left_click"):
+		posToGoTo = get_global_mouse_position()
+
+func _physics_process(delta: float) -> void:
+	var vel = posToGoTo - self.global_position
+	vel = vel.clamp(Vector2(-100, -100), Vector2(100, 100))
+	velocity = vel
+	move_and_slide()
+	
