ff44951384d33d6384b58c9be10c4efb930b6c69
Katie-N <kaitlyn.nordberg@gmail.com>
2025-10-30 18:08:12 -0500

    Added velocity check so that if the player runs into the wall, they will eventually idle

--- godot-game/src/player.gd
+++ godot-game/src/player.gd
@@ -11,20 +11,21 @@ func _input(event: InputEvent) -> void:
 		posToGoTo = get_global_mouse_position()
 
 func _physics_process(delta: float) -> void:
-	#var vel = posToGoTo - self.global_position
-	#vel = vel.clamp(Vector2(-100, -100), Vector2(100, 100))
-	#velocity = vel
-	
 	velocity = position.direction_to(posToGoTo) * speed
+	
 	if position.distance_to(posToGoTo) > 5:
 		move_and_slide()
-		if position.direction_to(posToGoTo)[0] > 0.00:
-			print("right")
-			$AnimationPlayer.play("player_walk_right")
+		print(velocity)
+		if abs(velocity[0]) < 1 and abs(velocity[1]) < 1:
+			print("Not Moving")
+			$AnimationPlayer.play("player_idle")
 		else:
-			print("left")
-			$AnimationPlayer.play("player_walk_left")
+			if position.direction_to(posToGoTo)[0] > 0.00:
+				print("right")
+				$AnimationPlayer.play("player_walk_right")
+			elif position.direction_to(posToGoTo)[0] <= 0.00:
+				print("left")
+				$AnimationPlayer.play("player_walk_left")
 	else: 
 		print("Not moving")
 		$AnimationPlayer.play("player_idle")
-	
