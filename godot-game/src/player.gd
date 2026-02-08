6816fe8b7fe5c5e3e8a284a690c5f0e2f3067368
Katie-N <kaitlyn.nordberg@gmail.com>
2026-02-07 18:33:24 -0600

    Made walking a bit better

--- godot-game/src/player.gd
+++ godot-game/src/player.gd
@@ -1,7 +1,11 @@
 extends CharacterBody2D
 
+@export var speed = 650
+@export var acceleration = 30  # How quickly you reach target speed
+@export var friction = 0.3    # How much velocity is retained (1.0 = no friction, 0.0 = instant stop)
+@export var stoppingDistance = 20
+
 var posToGoTo : Vector2
-var speed = 700
 var controlMode = "mouse"
 # In the future, implement controlMode = "keyboard" for wasd controls.
 	
@@ -13,22 +17,25 @@ func _unhandled_input(event: InputEvent) -> void:
 		posToGoTo = get_global_mouse_position()
 
 func _physics_process(delta: float) -> void:
-	velocity = global_position.direction_to(posToGoTo) * speed
+	var direction = global_position.direction_to(posToGoTo)
+	var target_velocity = direction * speed
 	
-	if global_position.distance_to(posToGoTo) > 5:
+	if global_position.distance_to(posToGoTo) > stoppingDistance:
+		# Gradual acceleration toward target velocity
+		velocity = velocity.lerp(target_velocity, acceleration * delta)
 		move_and_slide()
 		
-		#print(velocity)
-		if abs(velocity.x) < 1 and abs(velocity.y) < 1:
-			#print("Not Moving")
+		# Animation logic
+		if abs(velocity.x) < 50 and abs(velocity.y) < 50:
 			$AnimationPlayer.play("player_idle")
 		else:
-			if global_position.direction_to(posToGoTo)[0] > 0.00:
-				#print("right")
+			if direction.x > 0.00:
 				$AnimationPlayer.play("player_walk_right")
-			elif global_position.direction_to(posToGoTo)[0] <= 0.00:
-				#print("left")
+			elif direction.x <= 0.00:
 				$AnimationPlayer.play("player_walk_left")
-	else: 
-		#print("Not moving")
+	else:
+		# Apply friction when near target
+		velocity *= friction
+		if velocity.length() > 1:
+			move_and_slide()
 		$AnimationPlayer.play("player_idle")
