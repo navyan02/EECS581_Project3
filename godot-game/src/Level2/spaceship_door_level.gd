00c00728840297a3d0b408447cedabcfbc5a4e8d
Katie-N <kaitlyn.nordberg@gmail.com>
2026-02-14 18:00:17 -0600

    display endscene when level2 finishes

--- godot-game/src/Level2/spaceship_door_level.gd
+++ godot-game/src/Level2/spaceship_door_level.gd
@@ -1,10 +1,22 @@
 extends Node2D
 
+var nonogramSolved = false
+
 func _ready() -> void:
 	get_node("SmallAccessButtons").play("Access Denied")
 	
 
 func _on_nonogram_puzzle_nonogram_solved() -> void:
+	nonogramSolved = true
 	get_node("SmallAccessButtons").play("Access Granted")
 	get_node("SmallGrid/AnimatedSprite2D").visible = true
-	pass # Replace with function body.
+
+signal finishLevel2
+
+func _on_door_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		if nonogramSolved:
+			finishLevel2.emit()
+			$Endscene.visible = true
+		else:
+			print("Access not granted")
--- godot-game/src/endscene.gd
+++ godot-game/src/endscene.gd
@@ -2,9 +2,8 @@ extends Node2D
 
 @export var next_scene_path = "res://src/Scene2/TakeoffScene.tscn"
 
-func _ready() -> void:
-	$AnimationPlayer.play("fade")
-
+func _on_spaceship_door_finish_level_2() -> void:
+		$AnimationPlayer.play("fade")
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "fade":
