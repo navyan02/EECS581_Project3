c8409fee8279e5e9a10fd858d0e6bbc5b2e6f9e0
Katie <57237449+Katie-N@users.noreply.github.com>
2026-03-05 20:33:10 -0600

    Added level select map

---
+++ godot-game/src/levelSelection/level_selection_map.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _on_level_1_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/OpeningScene/openingscene.tscn")
+
+
+func _on_level_2_pressed() -> void:
+	get_tree().change_scene_to_file("res://src/Scene2/TakeoffScene.tscn")
+
+
+func _on_level_3_pressed() -> void:
+	#get_tree().change_scene_to_file("res://src/Scene2/TakeoffScene.tscn")
+	pass
