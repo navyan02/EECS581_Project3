7c33c070d400771218ae85251f75d6b3d5857950
Katie <57237449+Katie-N@users.noreply.github.com>
2026-03-24 22:59:24 -0500

    Level 3 spaceship interior

--- godot-game/src/Level3/maze.gd
+++ godot-game/src/Level3/maze.gd
@@ -1,11 +1,9 @@
-extends Sprite2D
+extends Area2D
 
+func _ready():
+	input_event.connect(_on_input_event)
+	set_pickable(true)
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int):
+	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.is_pressed():
+		get_tree().change_scene_to_file("res://src/Level3/PacManGame.tscn")
