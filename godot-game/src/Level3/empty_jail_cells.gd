5b12fc435c68c5722cded963b3ee167bd3965afd
Katie <57237449+Katie-N@users.noreply.github.com>
2026-04-02 17:57:46 -0500

    Move camera

--- godot-game/src/Level3/empty_jail_cells.gd
+++ godot-game/src/Level3/empty_jail_cells.gd
@@ -7,5 +7,9 @@ func checkForAlien():
 	if interactedWithHologram and interactedWithJailCells:
 		print("enter alien")
 		$AlienAnim.play("Alien Catches Intern")
+		var tween = get_tree().create_tween()
+		var new_position = Vector2(2523.0, 729.0) # get this from the trigger
+		tween.tween_property($Camera2D, "position", new_position, 1)
+		tween.tween_property($Camera2D, "position", $Player.position, 0.5)
 	else:
 		print("missing interaction")
