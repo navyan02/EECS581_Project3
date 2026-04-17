e898642e29269a73292b04323c77715632a3887a
Katie <57237449+Katie-N@users.noreply.github.com>
2026-04-17 11:16:19 -0500

    Level begin and end scenes for 4 and 5

--- godot-game/src/Level5/level5Endscene.gd
+++ godot-game/src/Level5/level5Endscene.gd
@@ -1,10 +1,9 @@
 extends CanvasLayer
 
-@export var next_scene_path = "res://src/Level4/Level4Start.tscn"
+@export var next_scene_path = "res://src/credits.tscn"
 
 func _on_node_finish_level_1() -> void:
 	$AnimationPlayer.play("fade")
 
 func _on_button_pressed() -> void:
-	print("go to level 4")
 	get_tree().change_scene_to_file(next_scene_path)
