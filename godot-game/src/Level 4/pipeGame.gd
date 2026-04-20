610beabb02f7e29c6c3b515c52a385bdfadc92f3
Katie-N <kaitlyn.nordberg@gmail.com>
2026-04-20 13:45:50 -0500

    Fixed level 4 transition

--- godot-game/src/Level 4/pipeGame.gd
+++ godot-game/src/Level 4/pipeGame.gd
@@ -26,7 +26,7 @@ func win():
 
 func _on_animation_player_animation_finished(anim_name: StringName) -> void:
 	if anim_name == "flyby":
-		get_tree().change_scene_to_file("res://src/Level4/passwordPuzzle.tscn")
+		get_tree().change_scene_to_file("res://src/Level4/mothershipJail.tscn")
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
