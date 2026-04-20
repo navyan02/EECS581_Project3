3b94f937a5e2cd81a0aa2ba3aa0d51679b22f41f
Katie-N <kaitlyn.nordberg@gmail.com>
2026-04-20 10:55:47 -0500

    Added mothership jail scene that will launch the password puzzle

---
+++ godot-game/src/Level4/mothership_jail.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+func _on_area_2d_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
+		_show_dialog("I think I can get my team out using my translator! I just need to guess the password...")
+		
+
+func _show_dialog(text: String):
+	var dialog = get_tree().get_first_node_in_group("dialog")
+	if dialog and dialog.has_method("show_message"):
+		dialog.show_message(text)
+	else:
+		print("Dialog: ", text)
