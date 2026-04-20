fd1242287929744bf8a6479884bce66104521a36
Katie-N <kaitlyn.nordberg@gmail.com>
2026-04-20 17:37:12 -0500

    Alien comes and pleads case

--- godot-game/src/Level5/level_5.gd
+++ godot-game/src/Level5/level_5.gd
@@ -3,12 +3,14 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$AnimationPlayer.play("cellDoorsOpen")
+	await $AnimationPlayer.animation_finished
 	$AnimationPlayer.play("leaveTheCells")
 	await $AnimationPlayer.animation_finished
 	$AnimationPlayer.play("dialog")
+	await $AnimationPlayer.animation_finished
+	$AnimationPlayer.play("alienWalksIn")
 	
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
